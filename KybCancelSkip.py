7d52e76709c85ac9bf7cd357f8db7ef28d5f778a
DirkLee <372810477@qq.com>
2019-01-20 00:13:49 +0800

    installApp

---
+++ KybCancelSkip.py
@@ -0,0 +1,38 @@
+from appium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+desired_caps={}
+desired_caps['platformName']='Android'
+desired_caps['deviceName']='127.0.0.1:62025'
+desired_caps['platforVersion']='5.1.1'
+
+
+desired_caps['app']=r'F:\APK\kaoyanbang-3.3.4.236.apk'
+desired_caps['appPackage']='com.tal.kaoyan'
+desired_caps['appActivity']='com.tal.kaoyan.ui.activity.SplashActivity'
+
+desired_caps['noReset']='True'
+
+driver=webdriver.Remote('http://localhost:4723/wd/hub',desired_caps)
+#隐式等待
+driver.implicitly_wait(5)
+
+def check_cancelBtn():
+    print('check_can')
+    try:
+        cacelBtn=driver.find_element_by_id('android:id/button2')
+    except NoSuchElementException:
+        print('no CancelBtn')
+    else:cacelBtn.click()
+
+    def check_skipBtn():
+        print("check_skipBtn")
+        try:
+            skipBtn = driver.find_element_by_id('com.tal.kaoyan:id/tv_skip')
+        except NoSuchElementException:
+            print('no skipBtn')
+        else:
+            skipBtn.click()
+
+            check_cancelBtn()
+            check_skipBtn()
